3f17fb82de1629948bb7dc43dda850f9094e6259
jesusMava <jesusmacedo7@gmail.com>
2022-06-21 15:46:54 -0500

    Delete colaborator migrations

--- db/migrate/20220617160612_create_collaborators.rb
+++ db/migrate/20220617160612_create_collaborators.rb
@@ -3,9 +3,9 @@
 class CreateCollaborators < ActiveRecord::Migration[7.0]
   def change
     create_table :collaborators do |t|
-      t.string :name
-      t.string :email
-      t.string :uuid
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :uuid, null: false
 
       t.timestamps
     end
