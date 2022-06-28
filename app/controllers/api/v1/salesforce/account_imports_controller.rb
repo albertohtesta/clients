86f58e9bc3ba60318d60a8aebd96dcdbad862065
Jonathan Andrés Calderón Roa <jcalderon@arkusnexus.com>
2022-06-28 17:08:23 +0000

    Feature/change port and remove authentication

--- app/controllers/api/v1/salesforce/account_imports_controller.rb
+++ app/controllers/api/v1/salesforce/account_imports_controller.rb
@@ -4,8 +4,6 @@ module Api
   module V1
     module Salesforce
       class AccountImportsController < JwtAuthAppsController
-        before_action :authenticate_request
-
         def create
           @imported_accounts = Account.import(JSON.parse(resource_params, symbolize_names: true))
           render json: { accounts: @imported_accounts }, status: :created
--- test/controllers/api/v1/salesforce/account_imports_controller_test.rb
+++ test/controllers/api/v1/salesforce/account_imports_controller_test.rb
@@ -6,10 +6,29 @@ module Api
   module V1
     module Salesforce
       class AccountImportsControllerTest < ActionDispatch::IntegrationTest
-        test "should get unauthorized code" do
+        def correct_request(params = nil)
+          post api_v1_salesforce_account_imports_url, params: params || { accounts: @accounts }
+        end
+
+        setup do
           @accounts = File.read Rails.root.join("test", "files", "test_salesforce.json")
-          post api_v1_salesforce_account_imports_url, params: @accounts
-          assert_response :unauthorized
+          create(:account_status, { status: "New", status_code: "new_project" })
+        end
+
+        test "should get created status" do
+          correct_request
+          assert_response :created
+        end
+
+        test "should return saved accounts" do
+          correct_request
+          response_body = JSON.parse(response.body, symbolize_names: true)
+          assert response_body[:accounts][0][:salesforce_id].present?
+        end
+
+        test "should get unprocessable_entity code" do
+          correct_request({ account: [] })
+          assert_response :unprocessable_entity
         end
       end
     end
