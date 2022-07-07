ca699949fd16aa1dabba2c65ddf67f5f22134dc5
Jesus M. Vazquez <jesus.macedo@michelada.io>
2022-07-07 21:21:11 +0000

    chore: Add config credentials for deploys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,9 +9,9 @@ Rails.application.configure do
   config.cache_classes = true
 
   config.hosts << "localhost:4000"
-  config.hosts << "qa-core-api.nordhen.com"
   config.hosts << "norden-clients-integration.herokuapp.com"
-
+  config.hosts << "qa-clients-api.nordhen.com"
+  config.hosts << "qa-clients.nordhen.com"
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
