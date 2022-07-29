d92dabf0aedfe82dadbe3855d8a558b084ebbbd9
Alejandro Rebollar <alejandro.rebollar@michelada.io>
2022-07-29 12:36:50 -0500

    fix: apply rubocop's suggestions

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Survey < ApplicationRecord
   belongs_to :team
-  enum status: [:open, :closed]
-  enum period: [:month, :quarter, :year]
+  enum status: %i[open closed]
+  enum period: %i[month quarter year]
 end
