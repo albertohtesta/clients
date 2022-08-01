8e0574a6c1550f63988ffb3db4ecb528c591b39d
Alejandro Rebollar <alejandro.rebollar@michelada.io>
2022-08-01 14:41:47 -0500

    chore: create mock distributionController

---
+++ app/controllers/api/v1/team_morale_surveys/distributions_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module TeamMoraleSurveys
+      # surveys distribution controller
+      class DistributionsController < ApiController
+        skip_before_action :verify_token, :current_user, only: :create
+
+        def create
+          survey_monkey_response = true
+          if survey_monkey_response
+            render json: { data: "something truthy" }, status: :ok
+          else
+            render json: { errors: "something false" }, status: :bad_request
+          end
+        end
+
+        private
+          def distribution_params
+          end
+      end
+    end
+  end
+end
