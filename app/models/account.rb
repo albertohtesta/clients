16fc4994f16be1152dd609e0ebe2c2d41af41cd4
jesusMava <jesusmacedo7@gmail.com>
2022-08-02 18:15:02 -0500

    feat: Modify endpoint to get collaborator contact

--- app/models/account.rb
+++ app/models/account.rb
@@ -8,6 +8,8 @@ class Account < ApplicationRecord
   has_many :payments
   has_many :contacts
   has_many :metrics, as: :related
+  has_and_belongs_to_many :collaborators
+
 
   validates :account_uuid, :name, presence: true
 
