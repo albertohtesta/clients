6b24ecdd7441543cd1f25068bdae519a4e82099f
Alejandro Rebollar <alejandro.rebollar@michelada.io>
2022-08-03 13:51:54 -0500

    fix: add answers_detail field and rename survey_url field

--- test/factories/surveys.rb
+++ test/factories/surveys.rb
@@ -3,12 +3,13 @@
 FactoryBot.define do
   factory :survey do
     status { 0 }
-    survey_monkey_url { "www.surveymonkey.com" }
+    survey_url { "MyString" }
     requested_answers { 1 }
     current_answers { 1 }
-    deadline { "2022-07-29" }
+    deadline { "2022-08-03" }
     period { 1 }
     questions_detail { "" }
+    answers_detail { "" }
     team
   end
 end
