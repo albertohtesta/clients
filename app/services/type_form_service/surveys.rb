543216d472dea2cb6cde0b373ad528d6e5628572
Alejandro Rebollar <alejandro.rebollar@michelada.io>
2022-08-03 14:45:30 -0500

    feat: add webhooks endpoints

--- app/services/type_form_service/surveys.rb
+++ app/services/type_form_service/surveys.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 module TypeFormService
-  # API endpoints for surveys resources
+  # API endpoints for surveys
   class Surveys
+    # Returns every survey
     def all(options = {})
       Client.new.get("forms", query: options)
     end
 
     # Returns surveys' information with details
-    def find_with_details(form_id, options = {})
+    def find(form_id, options = {})
       Client.new.get("forms/#{form_id}", query: options)
     end
   end
