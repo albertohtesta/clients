db43e34839f69ebc1b01dd919b6b44c817002186
Aldo Enriquez <aldo.enriquez@michelada.io>
2022-08-05 11:15:11 -0500

    WIP: Endpoint check if  surveys can be triggered

--- app/controllers/api/v1/team_morale_surveys/distributions_controller.rb
+++ app/controllers/api/v1/team_morale_surveys/distributions_controller.rb
@@ -6,16 +6,33 @@ module Api
       # surveys distribution controller
       class DistributionsController < ApiController
         def create
-          survey_monkey_response = true
-          if survey_monkey_response
-            render json: { data: "something truthy" }, status: :ok
+          render json: { errors: "Team doesn't exist." }, status: :bad_request and return unless Team.where(:id => params[:team_id]).present?
+          collaborators = CollaboratorRepository.find_collaborators_by_project_id(params[:team_id])
+          if self.open_survey? && self.correct_info? && collaborators.any?
+            render json: 
+            { 
+              data: {
+                team_id: params[:team_id],
+                total_team_members: collaborators.count
+              } 
+            }, status: :ok
           else
-            render json: { errors: "something false" }, status: :bad_request
+            render json: { errors: @error_msg }, status: :bad_request
           end
         end
 
         private
-          def distribution_params
+          def open_survey?
+            team_surveys = Survey.where(:team_id => params[:team_id]).last
+            @error_msg = 'There is a survey OPEN for this team.' and return false if team_surveys.status == 'open'
+            return true
+          end
+
+          def correct_info?
+            @error_msg = 'The dateline MUST be greater than today.' and return false unless Date.strptime(params[:deadline], '%d/%m/%y') > Date.today
+            # @error_msg = 'The period ONLY CAN be monthly or quarter.' and return false if params[:period] > Date.today
+            # @error_msg = 'The year MUST NOT be in the past.' and return false if Date.strptime(params[:year], '%y/%m/%d') > Date.today
+            return true
           end
       end
     end
