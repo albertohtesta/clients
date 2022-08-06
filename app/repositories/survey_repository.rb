70c77d7f560f8e8357c884c9805e649970344534
Alberto Testa <albertohtesta@gmail.com>
2022-08-05 20:33:39 -0500

    refac: rubocop fixes

--- app/repositories/survey_repository.rb
+++ app/repositories/survey_repository.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class SurveyRepository < ApplicationRepository
-    class << self
-      def surveys_by_team_dates_status(team_id, initial_date, end_date, status)
-        scope.where("team_id = :team_id and deadline >= :initial_date and deadline <= :end_date and status = :status", 
-                    team_id: team_id, initial_date: initial_date, end_date: end_date, status: status).
-                    select(:id, "questions_detail -> 'questions' AS questions")
-      end
+  class << self
+    def surveys_by_team_dates_status(team_id, initial_date, end_date, status)
+      scope.where("team_id = :team_id and deadline >= :initial_date and deadline <= :end_date and status = :status",
+                  team_id:, initial_date:, end_date:, status:).
+                  select(:id, "questions_detail -> 'questions' AS questions")
     end
   end
+end
--- test/factories/surveys.rb
+++ test/factories/surveys.rb
@@ -9,102 +9,102 @@ FactoryBot.define do
     deadline { Date.today + 1.months }
     period { 1 }
     questions_detail {
-      {
-        questions: [
-          { 
-            title: 'Pregunta 1',
-            category: 'Balance de vida',
-            final_score: 100,
-            answers: [
-              {
-                title: 'respuesta 1',
-                responses: 10,
-                score: 100
-              },
-              {
-                title: 'respuesta 2',
-                responses: 0,
-                score: 0
-              },
-      
-              {
-                title: 'respuesta 3',
-                responses: 0,
-                score: 0
-              }
-            ]
-          },
-          { 
-            title: 'Pregunta 2',
-            category: 'Balance de vida',
-            final_score: 100,
-            answers: [
-              {
-                title: 'respuesta 1',
-                responses: 10,
-                score: 100
-              },
-              {
-                title: 'respuesta 2',
-                responses: 0,
-                score: 0
-              },
-      
-              {
-                title: 'respuesta 3',
-                responses: 0,
-                score: 0
-              }
-            ]
-          },
-          { 
-            title: 'Pregunta 3',
-            category: 'Orgullo',
-            final_score: 100,
-            answers: [
-              {
-                title: 'respuesta 1',
-                responses: 10,
-                score: 100
-              },
-              {
-                title: 'respuesta 2',
-                responses: 0,
-                score: 0
-              },
-      
-              {
-                title: 'respuesta 3',
-                responses: 0,
-                score: 0
-              }
-            ]
-          },
-          { 
-            title: 'Pregunta 4',
-            category: 'Orgullo',
-            final_score: 100,
-            answers: [
-              {
-                title: 'respuesta 1',
-                responses: 10,
-                score: 100
-              },
-              {
-                title: 'respuesta 2',
-                responses: 0,
-                score: 0
-              },
-      
-              {
-                title: 'respuesta 3',
-                responses: 0,
-                score: 0
-              }
-            ]
-          } 
-        ]
-    }
+    {
+      questions: [
+        {
+          title: "Pregunta 1",
+          category: "Balance de vida",
+          final_score: 100,
+          answers: [
+            {
+              title: "respuesta 1",
+              responses: 10,
+              score: 100
+            },
+            {
+              title: "respuesta 2",
+              responses: 0,
+              score: 0
+            },
+
+            {
+              title: "respuesta 3",
+              responses: 0,
+              score: 0
+            }
+          ]
+        },
+        {
+          title: "Pregunta 2",
+          category: "Balance de vida",
+          final_score: 100,
+          answers: [
+            {
+              title: "respuesta 1",
+              responses: 10,
+              score: 100
+            },
+            {
+              title: "respuesta 2",
+              responses: 0,
+              score: 0
+            },
+
+            {
+              title: "respuesta 3",
+              responses: 0,
+              score: 0
+            }
+          ]
+        },
+        {
+          title: "Pregunta 3",
+          category: "Orgullo",
+          final_score: 100,
+          answers: [
+            {
+              title: "respuesta 1",
+              responses: 10,
+              score: 100
+            },
+            {
+              title: "respuesta 2",
+              responses: 0,
+              score: 0
+            },
+
+            {
+              title: "respuesta 3",
+              responses: 0,
+              score: 0
+            }
+          ]
+        },
+        {
+          title: "Pregunta 4",
+          category: "Orgullo",
+          final_score: 100,
+          answers: [
+            {
+              title: "respuesta 1",
+              responses: 10,
+              score: 100
+            },
+            {
+              title: "respuesta 2",
+              responses: 0,
+              score: 0
+            },
+
+            {
+              title: "respuesta 3",
+              responses: 0,
+              score: 0
+            }
+          ]
+        }
+      ]
+  }
   }
     answers_detail { "" }
     team
