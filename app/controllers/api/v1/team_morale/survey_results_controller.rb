680597622d1948ae9e29fea881ed581862f719a6
Alberto Testa <albertohtesta@gmail.com>
2022-08-09 19:54:06 -0500

    refac: eliminating params[:type] used for debug purpose

--- app/controllers/api/v1/team_morale/survey_results_controller.rb
+++ app/controllers/api/v1/team_morale/survey_results_controller.rb
@@ -6,7 +6,7 @@ module Api
       class SurveyResultsController < ApplicationController
         def index
           results = SurveyResultsService.get_surveys(params[:initial_month], params[:end_month], params[:year],
-                    params[:team_id], params[:type])
+                    params[:team_id])
           render json: results, status: :ok
         end
       end
