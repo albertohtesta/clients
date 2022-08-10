ae1302cc7c6e981abddcfded97ec7b926a13eec1
Alberto Testa <albertohtesta@gmail.com>
2022-08-10 14:48:25 -0500

    refac: include quarterly and years average

--- app/controllers/api/v1/team_morale/survey_results_controller.rb
+++ app/controllers/api/v1/team_morale/survey_results_controller.rb
@@ -4,11 +4,25 @@ module Api
   module V1
     module TeamMorale
       class SurveyResultsController < ApplicationController
+        before_action :validate_required_params, :validate_period, only: %i[ index ]
         def index
-          results = SurveyResultsService.get_surveys(params[:initial_month], params[:end_month], params[:year],
+          results = SurveyResultsService.get_surveys(params[:period], params[:year],
                     params[:team_id])
           render json: results, status: :ok
         end
+
+        private
+          def validate_period
+            if !params[:period].to_i.between?(0, 2)
+              render json: { message: "Period invalid" }, status: :bad_request
+            end
+          end
+
+          def validate_required_params
+            params.require([:period, :year, :team_id])
+          rescue ActionController::ParameterMissing
+            render json: { message: "Parameters missing" }, status: :bad_request
+          end
       end
     end
   end
