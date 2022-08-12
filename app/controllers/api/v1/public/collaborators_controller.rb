14e8ae128f33aa64ad6952a817de30a449cb73de
Rodrigo Uri Pérez Millán <rodritj01@gmail.com>
2022-08-12 16:34:08 -0500

    feat: calling the filter method directly

--- app/controllers/api/v1/public/collaborators_controller.rb
+++ app/controllers/api/v1/public/collaborators_controller.rb
@@ -11,7 +11,7 @@ module Api
 
           return render(json: { message: "No pool directory found" }, status: :not_found) if pool_directory.empty?
 
-          pool_directory = collaborators_filtered(params[:category]) if params[:category].present?
+          pool_directory = CollaboratorRepository.filter_by_category(params[:category]) if params[:category].present?
 
           render json: pool_directory, status: :ok
         end
@@ -31,10 +31,6 @@ module Api
 
           def collaborators_public_pool_directory
             CollaboratorRepository.collaborators_pool_directory
-          end
-
-          def collaborators_filtered(category)
-            CollaboratorRepository.filter_by_category(category)
           end
       end
     end
