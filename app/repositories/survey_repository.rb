5030a8eb81a0f67df7ac16ebb6b9894d896ccd38
Alberto Testa <albertohtesta@gmail.com>
2022-08-14 22:57:46 -0500

    refac: using created_at instead of deadline for results querys

--- app/repositories/survey_repository.rb
+++ app/repositories/survey_repository.rb
@@ -3,7 +3,7 @@
 class SurveyRepository < ApplicationRepository
   class << self
     def surveys_by_team_dates_status(team_id, initial_date, end_date, status)
-      scope.where("team_id = :team_id and deadline >= :initial_date and deadline <= :end_date and status = :status",
+      scope.where("team_id = :team_id and created_at >= :initial_date and created_at <= :end_date and status = :status",
                   team_id:, initial_date:, end_date:, status:).
                   select(:id, "questions_detail -> 'questions' AS questions")
     end
