54e58c872cee72f66eb5050d124c5e9abd0567be
Aldo Enriquez <aldo.enriquez@michelada.io>
2022-08-17 17:09:48 -0500

    Git rebase done

--- app/controllers/api/v1/team_morale/surveys_controller.rb
+++ app/controllers/api/v1/team_morale/surveys_controller.rb
@@ -4,6 +4,15 @@ module Api
   module V1
     module TeamMorale
       class SurveysController < ApplicationController
+        def create
+          @survey = Survey.new(survey_params.merge(status: "preparation"))
+          if @survey.save
+            render json: SurveyPresenter.new(@survey).json, status: :ok
+          else
+            render json: { errors: @survey.errors.full_messages }, status: :bad_request
+          end
+        end
+
         def index
           @surveys = TypeFormService::Surveys.new
           render json: @surveys.all
@@ -13,6 +22,11 @@ module Api
           @surveys = TypeFormService::Surveys.new
           render json: @surveys.find(params[:id])
         end
+
+        private
+          def survey_params
+            params.require(:survey).permit(:team_id, :deadline, :period)
+          end
       end
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -67,7 +67,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_170254) do
     t.integer "productivity", default: 0
     t.integer "speed", default: 0
     t.datetime "deleted_at", precision: nil
-    t.date "manager_started_date"
     t.index ["account_status_id"], name: "index_accounts_on_account_status_id"
     t.index ["manager_id"], name: "index_accounts_on_manager_id"
   end
@@ -274,6 +273,24 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_170254) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "surveys", force: :cascade do |t|
+    t.integer "status"
+    t.string "survey_url"
+    t.integer "requested_answers"
+    t.integer "current_answers"
+    t.date "deadline"
+    t.integer "period"
+    t.jsonb "questions_detail"
+    t.bigint "team_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.jsonb "answers_detail"
+    t.index ["answers_detail"], name: "index_surveys_on_answers_detail", using: :gin
+    t.index ["period"], name: "index_surveys_on_period"
+    t.index ["questions_detail"], name: "index_surveys_on_questions_detail", using: :gin
+    t.index ["team_id"], name: "index_surveys_on_team_id"
+  end
+
   create_table "team_requirements", force: :cascade do |t|
     t.bigint "account_id", null: false
     t.bigint "team_id", null: false
@@ -295,24 +312,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_170254) do
     t.datetime "updated_at", null: false
     t.index ["team_requirement_id"], name: "index_team_requirements_tech_stacks_on_team_requirement_id"
     t.index ["tech_stack_id"], name: "index_team_requirements_tech_stacks_on_tech_stack_id"
-  end
-
-  create_table "surveys", force: :cascade do |t|
-    t.integer "status"
-    t.string "survey_url"
-    t.integer "requested_answers"
-    t.integer "current_answers"
-    t.date "deadline"
-    t.integer "period"
-    t.jsonb "questions_detail"
-    t.bigint "team_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.jsonb "answers_detail"
-    t.index ["answers_detail"], name: "index_surveys_on_answers_detail", using: :gin
-    t.index ["period"], name: "index_surveys_on_period"
-    t.index ["questions_detail"], name: "index_surveys_on_questions_detail", using: :gin
-    t.index ["team_id"], name: "index_surveys_on_team_id"
   end
 
   create_table "team_types", force: :cascade do |t|
@@ -359,11 +358,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_170254) do
   add_foreign_key "posts", "collaborators"
   add_foreign_key "posts", "projects"
   add_foreign_key "projects", "accounts"
+  add_foreign_key "surveys", "teams"
   add_foreign_key "team_requirements", "accounts"
   add_foreign_key "team_requirements", "collaborators"
   add_foreign_key "team_requirements", "roles"
   add_foreign_key "team_requirements", "teams"
-  add_foreign_key "surveys", "teams"
   add_foreign_key "teams", "projects"
   add_foreign_key "teams", "team_types"
 end
