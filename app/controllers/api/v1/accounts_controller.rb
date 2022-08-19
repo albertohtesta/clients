c477c5fbe7cc74b172943e3bc53424a7b0a065fe
Denis Lafontant <icebox827@gmail.com>
2022-08-19 16:35:48 -0400

    feat: Finalize account controller

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -3,7 +3,17 @@
 module Api
   module V1
     class AccountsController < ApiController
-      def show; end
+      before_action :accounts_by_managers, only: :index
+      def index
+        return render json: { message: "Accounts not found" }, status: :not_found if @accounts.empty?
+
+        render json: AccountPresenter.json_collection(@accounts), status: :ok
+      end
+
+      private
+        def accounts_by_managers
+          @accounts = AccountRepository.by_manager(params[:manager_id])
+        end
     end
   end
 end
