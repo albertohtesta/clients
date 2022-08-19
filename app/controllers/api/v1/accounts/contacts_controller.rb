1c2235cb1867a6e880cecdb29e83820599e9d21b
Denis Lafontant <icebox827@gmail.com>
2022-08-19 16:52:41 -0400

    feat: Update contact controller

--- app/controllers/api/v1/accounts/contacts_controller.rb
+++ app/controllers/api/v1/accounts/contacts_controller.rb
@@ -2,25 +2,27 @@
 
 module Api
   module V1
-    class ContactsController < ApplicationController
-      before_action :retrieve_contacts, only: %i[index show]
+    module Accounts
+      class ContactsController < ApplicationController
+        before_action :retrieve_contacts, only: %i[index show]
 
-      def index
-        return render json: { message: "Contacts not found" }, status: :not_found if @account_info.empty?
+        def index
+          return render json: { message: "Contacts not found" }, status: :not_found if @account_info.empty?
 
-        render json: AccountContactCollaboratorPresenter.json_collection(@account_info), status: :ok
-      end
-
-      def show
-        return render json: { message: "Contacts not found" }, status: :not_found if @account_info.empty?
-
-        render json: AccountContactCollaboratorPresenter.json_collection(@account_info), status: :ok
-      end
-
-      private
-        def retrieve_contacts
-          @account_info = AccountContactCollaboratorRepository.contacts_by_account(params[:account_id])
+          render json: AccountContactCollaboratorPresenter.json_collection(@account_info), status: :ok
         end
+
+        def show
+          return render json: { message: "Contacts not found" }, status: :not_found if @account_info.empty?
+
+          render json: AccountContactCollaboratorPresenter.json_collection(@account_info), status: :ok
+        end
+
+        private
+          def retrieve_contacts
+            @account_info = AccountContactCollaboratorRepository.contacts_by_account(params[:account_id])
+          end
+      end
     end
   end
 end
