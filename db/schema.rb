d534d8ee45123270cd56cf1bb7183b91858b92a5
Bryan <bryan.guerrero@michelada.io>
2022-08-19 12:11:05 -0500

    chore: Add fields to invite account contacts

--- db/schema.rb
+++ db/schema.rb
@@ -171,6 +171,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_02_031158) do
     t.datetime "deleted_at", precision: nil
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "invite_status"
+    t.date "invite_date"
+    t.string "contact_uuid"
     t.index ["account_id"], name: "index_contacts_on_account_id"
   end
 
