9e45314f1b28a9594a6fcf3dbfa9d44832bbb773
Alberto Testa <albertohtesta@gmail.com>
2022-08-22 12:59:04 -0500

    fix: survey seed and factory

--- spec/factories/surveys.rb
+++ spec/factories/surveys.rb
@@ -2,109 +2,13 @@
 
 FactoryBot.define do
   factory :survey do
-    status { 2 }
-    survey_url { "www.survey.com" }
-    requested_answers { 10 }
-    current_answers { 10 }
-    deadline { Date.today + 1.months }period { 1 }
-    questions_detail {
-    {
-      questions: [
-        {
-          title: "Pregunta 1",
-          category: "Balance de vida",
-          final_score: 100,
-          answers: [
-            {
-              title: "respuesta 1",
-              responses: 10,
-              score: 100
-            },
-            {
-              title: "respuesta 2",
-              responses: 0,
-              score: 0
-            },
-
-            {
-              title: "respuesta 3",
-              responses: 0,
-              score: 0
-            }
-          ]
-        },
-        {
-          title: "Pregunta 2",
-          category: "Balance de vida",
-          final_score: 100,
-          answers: [
-            {
-              title: "respuesta 1",
-              responses: 10,
-              score: 100
-            },
-            {
-              title: "respuesta 2",
-              responses: 0,
-              score: 0
-            },
-
-            {
-              title: "respuesta 3",
-              responses: 0,
-              score: 0
-            }
-          ]
-        },
-        {
-          title: "Pregunta 3",
-          category: "Orgullo",
-          final_score: 100,
-          answers: [
-            {
-              title: "respuesta 1",
-              responses: 10,
-              score: 100
-            },
-            {
-              title: "respuesta 2",
-              responses: 0,
-              score: 0
-            },
-
-            {
-              title: "respuesta 3",
-              responses: 0,
-              score: 0
-            }
-          ]
-        },
-        {
-          title: "Pregunta 4",
-          category: "Orgullo",
-          final_score: 100,
-          answers: [
-            {
-              title: "respuesta 1",
-              responses: 10,
-              score: 100
-            },
-            {
-              title: "respuesta 2",
-              responses: 0,
-              score: 0
-            },
-
-            {
-              title: "respuesta 3",
-              responses: 0,
-              score: 0
-            }
-          ]
-        }
-      ]
-  }
-  }
+    status { 0 }
+    survey_url { "MyString" }
+    requested_answers { 1 }
+    current_answers { 1 }
+    deadline { "2022-09-29" }
+    period { 1 }
+    questions_detail { "" }
     answers_detail { "" }
     team
   end
