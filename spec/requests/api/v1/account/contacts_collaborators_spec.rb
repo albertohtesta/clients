1be2015b95687dcc84ae68416c6877800202f939
Denis Lafontant <icebox827@gmail.com>
2022-08-23 17:00:38 -0400

    fix: Update contact collaboratrs test

--- spec/requests/api/v1/account/contacts_collaborators_spec.rb
+++ spec/requests/api/v1/account/contacts_collaborators_spec.rb
@@ -27,4 +27,17 @@ RSpec.describe "Contacts Collaborators", type: :request do
       end
     end
   end
+  # describe "contacts collaborators" do
+  #   it "When the count have contacts" do
+  #     @account = create(:account)
+  #     @contact = create(:account_contact_collaborator, account: @account)
+  #     get api_v1_account_contacts_collaborators_path(account_id: @account.id), headers: { "Authorization" => @token }
+  #     expect(response).to have_http_status(200)
+  #   end
+
+  #   it "When the count doesnt have contacts" do
+  #     get api_v1_account_contacts_collaborators_path(account_id: 1), headers: { "Authorization" => @token }
+  #     expect(response).to have_http_status(404)
+  #   end
+  # end
 end
