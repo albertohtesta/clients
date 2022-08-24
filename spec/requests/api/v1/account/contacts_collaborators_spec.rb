caf9322e17e287f7e8805d11e0d33abce8587052
Denis Lafontant <icebox827@gmail.com>
2022-08-24 10:34:20 -0400

    fix: update swagger endpoints documentation

--- spec/requests/api/v1/account/contacts_collaborators_spec.rb
+++ spec/requests/api/v1/account/contacts_collaborators_spec.rb
@@ -15,39 +15,14 @@ RSpec.describe "Accounts", type: :request do
         security [ Bearer: [] ]
         consumes "application/json"
         produces "application/json"
-        parameter name: :account_id, in: :path, type: :string, description: "id of the account"
-
-        response "200", "contacts collaborators found" do
-          let(:account_id) { account.id }
-
-          run_test!
-        end
-      end
-    end
-
-    path "/api/v1/accounts/{account_id}/contacts_collaborators" do
-      get "Get all collaborators contact from an acccount" do
-        tags "Accounts"
-
-        security [ Bearer: [] ]
-        consumes "application/json"
-        produces "application/json"
         parameter name: :account_id, in: :path, type: :integer, description: "id of the account"
 
         response "404", "contacts collaborators not found" do
-          let(:account_id) {  @account }
+          let(:account_id) {  "123" }
 
           run_test!
         end
       end
     end
   end
-  # describe "contacts collaborators" do
-  #   it "When the count have contacts" do
-  #     @account = create(:account)
-  #     @contact = create(:account_contact_collaborator, account: @account)
-  #     get api_v1_account_contacts_collaborators_path(account_id: @account.id), headers: { "Authorization" => @token }
-  #     expect(response).to have_http_status(200)
-  #   end
-  # end
 end
