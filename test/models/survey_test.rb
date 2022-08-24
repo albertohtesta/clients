aa1b5fe10beec1480f4e7aafe838dbb877cd5035
Bryan <bryan.guerrero@michelada.io>
2022-08-24 14:25:07 -0500

    Revert "chore: Merge branch 'chore/add-seeds-real-data' into develop"
    This reverts commit e4d394a11cb282df86c6c3a006d71e9aa1b49cfc, reversing
    changes made to 7f6a10ca4ee4fbbaa44169163ae375e9a906a7e9.

--- test/models/survey_test.rb
+++ test/models/survey_test.rb
@@ -28,23 +28,4 @@ class SurveyTest < ActiveSupport::TestCase
     @survey = build_stubbed(:survey)
     assert @survey.valid?
   end
-  test "can not create a survey without a team" do
-    assert_not build_stubbed(:survey, team: nil).valid?
-  end
-
-  test "can get collaborators" do
-    team = create(:team)
-    collaborator = create(:collaborator)
-    team.collaborators << collaborator
-    survey = create(:survey, team:)
-    assert_equal(survey.team.collaborators.size, 1)
-  end
-
-  test "can get collaborators email" do
-    team = create(:team)
-    collaborator = create(:collaborator)
-    team.collaborators << collaborator
-    survey = create(:survey, team:)
-    assert_not_nil(survey.team.collaborators.first.email)
-  end
 end
