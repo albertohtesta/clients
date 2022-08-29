f4dfa6d273a277b49174761e50d9a5fc5aa5ced5
iosorio@arkusnexus.com <iosorio@arkusnexus.com>
2022-08-29 14:58:02 -0700

    feat: add core urls to allow origin list

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,8 +5,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "qa-clients.nordhen.com",
             "localhost:4001",
             "arkusimpel.my.salesforce.com",
-            "arkusimpel--restapidev.sandbox.lightning.force.com"
-
+            "arkusimpel--restapidev.sandbox.lightning.force.com",
+            "https://qa-core.nordhen.com",
+            "https://staging-core.nordhen.com",
+            "https://core.nordhen.com",
+            "https://qa-core-api.nordhen.com"
     resource "*",
       headers: :any,
       methods: %i[get post put patch delete options head]
