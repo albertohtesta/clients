c6a7bcc312d04651ad0c0dbdb8f777ac369cb61f
Denis Lafontant <icebox827@gmail.com>
2022-09-01 19:08:53 -0400

    fix: Fix bugs in balance calculations

--- app/controllers/api/v1/team_balance/balances_controller.rb
+++ app/controllers/api/v1/team_balance/balances_controller.rb
@@ -5,34 +5,16 @@ module Api
     module TeamBalance
       class BalancesController < ApplicationController
         before_action :retrieve_teams, only: :index
-        before_action :retrieve_team_by_id, only: :create
         def index
-          render json: { message: "No balance found" }, status: :not_found if @teams.nil
+          render json: { message: "No balance found" }, status: :not_found if  @team_balances.nil
 
-          render json: TeamBalancePresenter.json_collection(@teams), status: :ok
+          render json: TeamBalancePresenter.json_collection(@team_balances), status: :ok
         end
 
-        def create
-          @team_balance = @team.TeamBalanceRepository.new_entity(team_balance_params)
-
-          if @team_balance.save
-            render json: @team_balance, status: :created
-          else
-            render json: @team_balance, status: :unprocessable_entity
-          end
-        end
 
         private
-          def retrieve_teams
-            @teams = TeamBalanceRepository.all
-          end
-
-          def retrieve_team_by_id
-            @team = TeamBalanceRepository.balance_by_team(param[:team_id])
-          end
-
-          def team_balance_params
-            params.permit(:team_id, :balance, :balance_date)
+          def set_team_balance
+            @team_balances = TeamBalanceService.new(params[:team_id]).create
           end
       end
     end
