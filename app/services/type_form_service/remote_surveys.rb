5d68fe5663302a0351b80cf2b671a8b3b622e0d4
Alberto Testa <albertohtesta@gmail.com>
2022-09-02 16:47:04 -0500

    feat: check nil in response

--- app/services/type_form_service/remote_surveys.rb
+++ app/services/type_form_service/remote_surveys.rb
@@ -17,7 +17,7 @@ module TypeFormService
     def self.create
       type_form_survey_template = File.join(Rails.root, "app", "services/type_form_service", "typeform_survey_template.txt")
       data = HttpClient.new.post("forms", JSON.parse(File.read(type_form_survey_template)))
-      data = safe_json(data)
+      data = safe_json(data) unless data.blank?
       return { typeform_survey_id: data[:id], typeform_survey_url: data[:_links][:display] } unless data.blank?
 
       { error: "survey not created" }
