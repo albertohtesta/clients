ddef6a6fef7eaff24b437f3c84d439d820d17994
Denis Lafontant <icebox827@gmail.com>
2022-09-02 19:34:55 -0400

    feat: Create test for balance controller

--- spec/requests/api/v1/team_balance/balances_spec.rb
+++ spec/requests/api/v1/team_balance/balances_spec.rb
@@ -3,17 +3,22 @@
 require "swagger_helper"
 
 RSpec.describe "api/v1/team_balance/balances", type: :request do
-  path "/api/v1/team_balance/balances" do
-    get("list balances") do
-      response(200, "successful") do
-        after do |example|
-          example.metadata[:response][:content] = {
-            "application/json" => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+  context "Team balances" do
+    let(:team) { create(:team) }
+
+    path "/api/v1/team_balance/balances" do
+      get("list balances") do
+        tags "Balances"
+
+        consumes "application/json"
+        produces "application/json"
+        parameter name: :team_id, in: :path, type: :integer, description: "id of the team"
+
+        response(200, "successful") do
+          let(:team_id) { Team.first }
+
+          run_test!
         end
-        run_test!
       end
     end
   end
