9a31a1edc2d44bac97d7f079f26e2caf9ff98ec9
Bryan <bryan.guerrero@michelada.io>
2022-09-05 13:05:20 -0500

    feature: add rollbar to manage error on qa

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,14 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+notify = ->(e) do
+  begin
+    Rollbar.with_config(use_async: false) do
+      Rollbar.error(e)
+    end
+  rescue
+    Rails.logger.error "Synchronous Rollbar notification failed.  Sending async to preserve info"
+    Rollbar.error(e)
+  end
+end
