c62d6b05a7109eab52532afeba404b3030847491
Bryan <bryan.guerrero@michelada.io>
2022-09-05 13:28:25 -0500

    fix: pipeline

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
 
-# Load the Rails application.
 require_relative "application"
+require_relative "initializers/rollbar"
 
-# Initialize the Rails application.
-Rails.application.initialize!
+notify = -> (e) do
+  begin
+    # To see more info about rollbar visit:
+    # https://hixonrails.com/ruby-on-rails-tutorials/ruby-on-rails-rollbar-logger-installation-and-configuration/
+    Rollbar.with_config(use_async: false) do
+      Rollbar.error(e)
+    end
+  rescue
+    Rails.logger.error "Synchronous Rollbar notification failed.  Sending async to preserve info"
+    Rollbar.error(e)
+  end
+end
 
+begin
+  Rails.application.initialize!
+rescue Exception => e
+  notify.call(e)
+  raise
+end
