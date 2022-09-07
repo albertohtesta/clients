8c2fcf42365e9c90cad12ebd807019d4bc52f612
Bryan <bryan.guerrero@michelada.io>
2022-09-07 17:28:36 -0500

    fix: api controller

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,28 +3,28 @@
 module Api
   module V1
     class ApiController < ApplicationController
-      before_action :access_token, :decode_token, :verify_token, :current_user
+      before_action :access_token, :verify_token, :current_user
 
       def access_token
         @access_token ||= request.headers["Authorization"]
       end
 
       def verify_token
-        render json: { message: "Invalid token" }, status: :unauthorized unless @data_token
+        render json: { message: "Invalid token" }, status: :unauthorized unless decoded_token
       end
 
       def current_user
-        if @data_token[0]["cognito:groups"].include?("admin")
+        if decoded_token[0]["cognito:groups"].include?("admin")
           contact = ContactService.new({ token: access_token })
           @current_user ||= ContactRepository.find_by({ uuid: contact.logged_contact_email })
-        elsif @data_token[0]["cognito:groups"].include?("collaborator")
+        elsif decoded_token[0]["cognito:groups"].include?("collaborator")
           collaborator = CollaboratorService.new({ token: access_token })
           @current_user ||= CollaboratorRepository.find_by({ email: collaborator.logged_user_email })
         end
       end
 
-      def decode_token
-        @data_token = TokenService.new({ token: access_token }).decode
+      def decoded_token
+        @data_token ||= TokenService.new({ token: access_token }).decode
       end
     end
   end
