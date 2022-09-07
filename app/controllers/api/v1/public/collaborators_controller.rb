260b662dc9cf0b3b030d4d2da7711d75e0e8f1ef
Rodrigo Uri Pérez Millán <rodritj01@gmail.com>
2022-09-07 17:43:44 -0500

    fix: calling talent_pool_directory using memoization

--- app/controllers/api/v1/public/collaborators_controller.rb
+++ app/controllers/api/v1/public/collaborators_controller.rb
@@ -26,7 +26,7 @@ module Api
           end
 
           def talent_pool_directory
-            CollaboratorRepository.collaborators_pool_directory(params[:account_id], params[:category])
+            @talent_pool_directory ||= CollaboratorRepository.collaborators_pool_directory(params[:account_id], params[:category])
           end
       end
     end
