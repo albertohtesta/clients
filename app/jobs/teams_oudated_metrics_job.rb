1d7de182d1d5414824af7378a2ed21ad4120e6e6
Bryan <bryan.guerrero@michelada.io>
2022-09-07 10:16:40 -0500

    chore: add recurrent scheduler

--- app/jobs/teams_oudated_metrics_job.rb
+++ app/jobs/teams_oudated_metrics_job.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class TeamsOudatedMetricsJob < ApplicationJob
-  # repeat "every week at 3am"
-  queue_as :default
-
   def perform
+    puts ">>Job Team metrics executing<<"
+
     teams_with_metrics = TeamRepository.all.includes(:metrics)
     teams_with_metrics.map do |team|
       data = team.metrics.where("extract(month from date) >= ?", 1.months.ago.month)
