0d2e447bc7191522fb0f485509492d45fb908fc8
Manuel Villa <jvilla@arkusnexus.com>
2022-09-09 16:22:52 -0700

    chore: NCM-346-investment-metrics-last-period

--- app/presenters/investment_presenter.rb
+++ app/presenters/investment_presenter.rb
@@ -11,13 +11,13 @@ class InvestmentPresenter < ApplicationPresenter
 
   class << self
     def order_by_quarters(investments)
-      quarters = prepare_items(investments, 4, "quarter")
+      quarters = prepare_last_period(investments, "quarter")
 
       prepare_hash(quarters, "quarter")
       end
 
     def order_by_months(investments)
-      months = prepare_items(investments, 12, "month")
+      months = prepare_last_period(investments, "month")
 
       prepare_hash(months, "month")
     end
@@ -31,16 +31,26 @@ class InvestmentPresenter < ApplicationPresenter
               "value" => value.round(2)
             }
           end
-          end
+        end
 
-        def prepare_items(investments, slots, type)
-          items = (1..slots).map { |n| { n => 0 } }.reduce(:merge)
-
-          investments.each do |investment|
-            presenter = InvestmentPresenter.new(investment)
-            items[presenter.send(type)] += presenter.value
-          end
-          items
+        def prepare_last_period(investments, type)
+          return {} if investments.nil? || investments.empty?
+          current_month = Time.now.month
+          current_quarter = (current_month / 3).ceil
+          quarter_first_month = (current_quarter - 1) * 3 + 1
+          # go for previous month, or current quarter
+          month_range = {
+            "month" => [current_month - 1],
+            "quarter" => (quarter_first_month..quarter_first_month + 2).to_a
+          }[type]
+          value_sum = investments
+              .select { |inv| month_range.include? inv.date.month }
+              .sum(&:value)
+          period_number = {
+            "month" => current_month - 1,
+            "quarter" => current_quarter
+          }[type]
+          { period_number => value_sum }
         end
   end
 end
--- test/controllers/api/v1/investments_controller_test.rb
+++ test/controllers/api/v1/investments_controller_test.rb
@@ -7,6 +7,14 @@ class Api::V1::Teams::InvestmentsControllerTest < ActionDispatch::IntegrationTes
     stub_cognito_uri
     @team = create(:team)
     @investment = create(:investment, { team: @team })
+    @month = Time.now.month
+    @quarter =
+      case @month
+      when 1, 2, 3 then "q1"
+      when 4, 5, 6 then "q2"
+      when 7, 8, 9 then "q3"
+      when 10, 11, 12 then "q4"
+      end
   end
 
   test "should get success response" do
@@ -14,14 +22,14 @@ class Api::V1::Teams::InvestmentsControllerTest < ActionDispatch::IntegrationTes
     assert_response :success
   end
 
-  test "should get investments by months" do
+  test "should get investments from previous month" do
     get api_v1_team_investments_organized_path(@investment.team.id, "months"), headers: { "Authorization" => @token }
-    assert_equal response.parsed_body["project_indicators"].first["label"], "January"
+    assert_equal response.parsed_body["project_indicators"].first["label"], (Date.today - 1.month).strftime("%B")
   end
 
-  test "should get investments by quarters" do
+  test "should get investments for current quarter" do
     get api_v1_team_investments_organized_path(@investment.team.id, "quarters"), headers: { "Authorization" => @token }
-    assert_equal response.parsed_body["project_indicators"].first["label"], "q1"
+    assert_equal response.parsed_body["project_indicators"].first["label"], @quarter
   end
 
   test "should get 'Team not found' error" do
