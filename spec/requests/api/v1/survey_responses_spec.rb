144775ab5c69755c618b19733b00762d79bda201
Alberto Testa <albertohtesta@gmail.com>
2022-09-11 12:29:58 -0500

    refac: refactoring the code according to comments in CR

--- spec/requests/api/v1/survey_responses_spec.rb
+++ spec/requests/api/v1/survey_responses_spec.rb
@@ -3,39 +3,37 @@
 require "swagger_helper"
 
 RSpec.describe "close survey" do
-  before(:each) do
-    @survey = create(:survey, id: 1, survey_url: "www.ficticia.com")
-  end
+  let(:survey) { create(:survey, id: 1, survey_url: "www.ficticia.com") }
 
   it "returns true if survey is closed" do
-    response = SurveyResponsesService.close_survey(@survey.id)
+    response = SurveyResponsesService.new(survey.id).close_survey
     expect(response).to be_truthy
   end
 
   it "close survey and change status" do
-    SurveyResponsesService.close_survey(@survey.id)
-    expect(@survey.reload.status).to eq("closed")
+    SurveyResponsesService.new(survey.id).close_survey
+    expect(survey.reload.status).to eq("closed")
   end
 
   it "do not close survey that is incomplete" do
-    @survey.current_answers = 0
-    @survey.requested_answers = 5
-    @survey.save
-    SurveyResponsesService.check_if_survey_should_be_closed(@survey.id)
-    expect(@survey.reload.status).not_to eq("closed")
+    survey.current_answers = 0
+    survey.requested_answers = 5
+    survey.save
+    SurveyResponsesService.new(survey.id).close_survey
+    expect(survey.reload.status).not_to eq("closed")
   end
 
   it "close survey after its deadline" do
-    @survey.current_answers = 2
-    @survey.requested_answers = 5
-    @survey.deadline = Date.today - 1
-    @survey.save
-    SurveyResponsesService.check_if_survey_should_be_closed(@survey.id)
-    expect(@survey.reload.status).to eq("closed")
+    survey.current_answers = 2
+    survey.requested_answers = 5
+    survey.deadline = Date.today - 1
+    survey.save
+    SurveyResponsesService.new(survey.id).close_survey
+    expect(survey.reload.status).to eq("closed")
   end
 
   it "returns false if survey not exist" do
-    response = SurveyResponsesService.close_survey(5000)
+    response = SurveyResponsesService.new(5000).close_survey
     expect(response).to be_falsey
   end
 end
