ce3665a1a99a17f7f555b928786b6240658d4d11
Alberto Testa <albertohtesta@gmail.com>
2022-09-14 14:18:15 -0500

    feat: migration for creating morale attributes in DB and refactoring

--- spec/requests/api/v1/survey_responses_spec.rb
+++ spec/requests/api/v1/survey_responses_spec.rb
@@ -3,33 +3,26 @@
 require "swagger_helper"
 
 RSpec.describe "close survey" do
-  before(:each) do
-    @survey = create(:survey, id: 1, survey_url: "www.ficticia.com")
-  end
+  let(:survey) { create(:survey, id: 1, survey_url: "www.ficticia.com") }
 
-  it "returns the survey if survey is closed" do
-    response = SurveyResponsesService.new(@survey.id).close_survey
+  it "returns the survey when closing it" do
+    response = SurveyResponsesService.new(survey.id).close_survey
     expect(response).to be_truthy
   end
 
-  it "close survey and change status" do
-    SurveyResponsesService.new(@survey.id).close_survey
-    expect(@survey.reload.status).to eq("closed")
+  it "closes the survey" do
+    expect { SurveyResponsesService.new(survey.id).close_survey }.to change { survey.reload.status }.from("preparation").to("closed")
   end
 
   it "do not close survey that is incomplete" do
-    @survey.current_answers = 0
-    @survey.requested_answers = 5
-    @survey.save
+    @survey = create(:survey, id: 1, survey_url: "www.ficticia.com", current_answers: 0, requested_answers: 5)
     SurveyResponsesService.new(@survey.id).close_survey
     expect(@survey.reload.status).not_to eq("closed")
   end
 
   it "close survey after its deadline" do
-    @survey.current_answers = 2
-    @survey.requested_answers = 5
-    @survey.deadline = Date.today - 1
-    @survey.save
+    @survey = create(:survey, id: 1, survey_url: "www.ficticia.com", current_answers: 2, requested_answers: 5)
+    @survey.update_columns(deadline: Date.today - 1)
     SurveyResponsesService.new(@survey.id).close_survey
     expect(@survey.reload.status).to eq("closed")
   end
