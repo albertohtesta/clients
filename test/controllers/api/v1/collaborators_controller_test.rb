87e3b2fe19c50345637c8034225552bc7323f46a
Bryan <bryan.guerrero@michelada.io>
2022-09-13 19:30:47 -0500

    fix: test with minitest errors

--- test/controllers/api/v1/collaborators_controller_test.rb
+++ test/controllers/api/v1/collaborators_controller_test.rb
@@ -1,18 +1,17 @@
 # frozen_string_literal: true
-# # frozen_string_literal: true
 
-# require "test_helper"
+require "test_helper"
 
-# class Api::V1::Teams::CollaboratorsControllerTest < ActionDispatch::IntegrationTest
-#   setup do
-#     stub_cognito_uri
-#   end
+class Api::V1::Teams::CollaboratorsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    stub_cognito_uri
+  end
 
-#   test "should get index" do
-#     params = { team_id: 1 }
-#     get api_v1_team_collaborators_path(params), headers: { "Authorization" => @token }
+  test "should get index" do
+    params = { team_id: 1 }
+    get api_v1_team_collaborators_path(params), headers: { "Authorization" => @token }
 
-#     assert_equal(response.parsed_body, { "message" => "No collaborators" })
-#     assert_response(:not_found)
-#   end
-# end
+    assert_equal(response.parsed_body, { "message" => "No collaborators" })
+    assert_response(:not_found)
+  end
+end
