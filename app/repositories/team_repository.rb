0b0520107fa86bdcfb39b907315e451723ff0d96
Bryan <bryan.guerrero@michelada.io>
2022-09-14 14:27:16 -0500

    fix: team repository search by account

--- app/repositories/team_repository.rb
+++ app/repositories/team_repository.rb
@@ -13,5 +13,9 @@ class TeamRepository < ApplicationRepository
     def find_all_collaborators_by_seniority(id, seniority)
       scope.find_by(id:).collaborators.where(seniority:)
     end
+
+    def find_teams_of_project(project_id)
+      scope.select(:id).find_by(project_id:)
+    end
   end
 end
