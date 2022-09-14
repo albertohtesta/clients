2fa9b3d26ff98d953bd49d526f35c4f143606933
Bryan <bryan.guerrero@michelada.io>
2022-09-14 18:26:04 -0500

    fix: rspec skiped test

--- spec/requests/api/v1/survey_results_spec.rb
+++ spec/requests/api/v1/survey_results_spec.rb
@@ -1,28 +1,27 @@
 # frozen_string_literal: true
-# # frozen_string_literal: true
 
-# require "swagger_helper"
+require "swagger_helper"
 
-# RSpec.describe "api/v1/team_morale/survey_results", type: :request do
-#   path "/api/v1/team_morale/survey_results" do
-#     get("list survey results") do
-#       tags "Surveys"
-#       let(:period) { "0" }
-#       let(:year) { "2022" }
-#       let(:team_id) { "1" }
-#       let(:processing_type) { "A" }
-#       parameter name: :period, in: :query, type: :string
-#       parameter name: :year, in: :query, type: :string
-#       parameter name: :team_id, in: :query, type: :string
-#       parameter name: :processing_type, in: :query, type: :string
-#       produces "application/json"
+RSpec.describe "api/v1/team_morale/survey_results", type: :request do
+  path "/api/v1/team_morale/survey_results" do
+    get("list survey results") do
+      tags "Surveys"
+      let(:period) { "0" }
+      let(:year) { "2022" }
+      let(:team_id) { "1" }
+      let(:processing_type) { "A" }
+      parameter name: :period, in: :query, type: :string
+      parameter name: :year, in: :query, type: :string
+      parameter name: :team_id, in: :query, type: :string
+      parameter name: :processing_type, in: :query, type: :string
+      produces "application/json"
 
-#       response 200, "successful" do
-#         run_test! do |response|
-#           expect(response.body).not_to be_empty
-#           expect(response).to have_http_status(:success)
-#         end
-#       end
-#     end
-#   end
-# end
+      response 200, "successful" do
+        run_test! do |response|
+          expect(response.body).not_to be_empty
+          expect(response).to have_http_status(:success)
+        end
+      end
+    end
+  end
+end
