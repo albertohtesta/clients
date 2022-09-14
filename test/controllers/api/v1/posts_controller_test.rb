eea500ab8dd3b76b94b8289eeebc27f9ab86e68d
Bryan <bryan.guerrero@michelada.io>
2022-09-13 19:30:47 -0500

    fix: test with minitest errors

--- test/controllers/api/v1/posts_controller_test.rb
+++ test/controllers/api/v1/posts_controller_test.rb
@@ -1,34 +1,33 @@
 # frozen_string_literal: true
-# # frozen_string_literal: true
 
-# require "test_helper"
+require "test_helper"
 
-# class Api::V1::PostsControllerTest < ActionDispatch::IntegrationTest
-#   setup do
-#     stub_cognito_uri
-#   end
+class Api::V1::PostsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    stub_cognito_uri
+  end
 
-#   test "should get success response" do
-#     test_post = create_fake_post
+  test "should get success response" do
+    test_post = create_fake_post
 
-#     get api_v1_post_path(id: test_post.id), headers: { "Authorization" => @token }
+    get api_v1_post_path(id: test_post.id), headers: { "Authorization" => @token }
 
-#     assert_response :success
-#   end
+    assert_response :success
+  end
 
-#   test "should get 'post not found' error" do
-#     get api_v1_post_path(id: 0), headers: { "Authorization" => @token }
+  test "should get 'post not found' error" do
+    get api_v1_post_path(id: 0), headers: { "Authorization" => @token }
 
-#     assert_response :not_found
-#   end
+    assert_response :not_found
+  end
 
-#   private
-#     def create_fake_post
-#       test_post = create(:post)
-#       test_post.post.attach(
-#         io: File.open(Rails.root.join("test/fixtures/files/example.png")),
-#         filename: "example.png"
-#       )
-#       test_post
-#     end
-# end
+  private
+    def create_fake_post
+      test_post = create(:post)
+      test_post.post.attach(
+        io: File.open(Rails.root.join("test/fixtures/files/example.png")),
+        filename: "example.png"
+      )
+      test_post
+    end
+end
--- test/controllers/api/v1/public/collaborators_controller_test.rb
+++ test/controllers/api/v1/public/collaborators_controller_test.rb
@@ -1,23 +1,22 @@
 # frozen_string_literal: true
-# # frozen_string_literal: true
 
-# require "test_helper"
+require "test_helper"
 
-# class Api::V1::Public::CollaboratorsControllerTest < ActionDispatch::IntegrationTest
-#   setup do
-#     stub_cognito_uri
-#     @collaborator = create(:collaborator)
-#   end
+class Api::V1::Public::CollaboratorsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    stub_cognito_uri
+    @collaborator = create(:collaborator)
+  end
 
-#   test "should show api_v1_public_collaborator info" do
-#     get api_v1_public_collaborator_path(@collaborator.id), headers: { "Authorization" => @token }
+  test "should show api_v1_public_collaborator info" do
+    get api_v1_public_collaborator_path(@collaborator.id), headers: { "Authorization" => @token }
 
-#     assert_response :success
-#   end
+    assert_response :success
+  end
 
-#   test "should get not found when collaborator not exist" do
-#     get api_v1_public_collaborator_url(0), headers: { "Authorization" => @token }
+  test "should get not found when collaborator not exist" do
+    get api_v1_public_collaborator_url(0), headers: { "Authorization" => @token }
 
-#     assert_response :not_found
-#   end
-# end
+    assert_response :not_found
+  end
+end
