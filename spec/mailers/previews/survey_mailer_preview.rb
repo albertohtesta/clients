feffbbc6025b418543fd43c01d453d326ef81ca5
Aldo Enriquez <aldo.enriquez@michelada.io>
2022-09-16 08:23:54 -0500

    Code review changes

--- spec/mailers/previews/survey_mailer_preview.rb
+++ spec/mailers/previews/survey_mailer_preview.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
-# Preview all emails at http://localhost:3000/rails/mailers/survey_mailer
 class SurveyMailerPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/survey_mailer/survey_created
   def survey_created
     SurveyMailer.with(
-      collaborator_name: "#{Survey.last.team.collaborators.first.first_name} #{Survey.last.team.collaborators.first.last_name}",
-      collaborator_email: Survey.last.team.collaborators.first.email, 
-      subject: Survey.last.description, 
+      collaborator_name: CollaboratorPresenter.new(Survey.last.team.collaborators.first).json["name"],
+      collaborator_email: Survey.last.team.collaborators.first.email,
+      subject: Survey.last.description,
       survey_url: Survey.last.survey_url
     ).survey_created
   end
