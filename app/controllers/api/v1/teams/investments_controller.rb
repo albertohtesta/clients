66c5d26dfb273a5e5b029d7863d0a36e65acfaf4
Bryan <bryan.guerrero@michelada.io>
2022-09-21 11:55:12 -0500

    chore: refactor investments and metrics to equalize

--- app/controllers/api/v1/teams/investments_controller.rb
+++ app/controllers/api/v1/teams/investments_controller.rb
@@ -4,23 +4,25 @@ module Api
   module  V1
     module Teams
       class InvestmentsController < ApplicationController
-        before_action :retrieve_team, only: :show
+        before_action :validate_required_params
 
-        def show
-          if investments = retrieve_investments_by_team_id
-            render json: { project_indicators: InvestmentPresenter.send("order_by_#{params[:order_by]}", investments) }
-          else
-            render json: { message: "No investments data found" }, status: :not_found
-          end
+        def index
+          investments = investments_of_team_of_this_year
+          return render json: { message: "No investments data found" }, status: :not_found unless investments
+
+          investments_formated = InvestmentPresenter.send("order_by_#{params[:group_by]}", investments)
+          render json: { team_indicators: investments_formated }
         end
 
         private
-          def retrieve_team
-            @team = TeamRepository.find_by(id: params[:team_id])
+          def investments_of_team_of_this_year
+            InvestmentRepository.retrieve_current_year_investments_by_team_id(params[:team_id])
           end
 
-          def retrieve_investments_by_team_id
-            InvestmentRepository.retrieve_current_year_investments_by_team_id(@team.id) if @team
+          def validate_required_params
+            params.require([:group_by])
+          rescue ActionController::ParameterMissing
+            render json: { message: "Parameters missing" }, status: :bad_request
           end
       end
     end
