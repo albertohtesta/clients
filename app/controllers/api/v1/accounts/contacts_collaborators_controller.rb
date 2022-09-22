1556031e71a7dcfa79d8ed5532a554372674baee
Bryan <bryan.guerrero@michelada.io>
2022-09-22 11:31:05 -0500

    fix: using memoization in controllers

--- app/controllers/api/v1/accounts/contacts_collaborators_controller.rb
+++ app/controllers/api/v1/accounts/contacts_collaborators_controller.rb
@@ -5,15 +5,14 @@ module Api
     module Accounts
       class ContactsCollaboratorsController < ApiController
         def index
-          contacts_collabs = retrieve_contacts
           return render json: { message: "Contacts not found" }, status: :not_found if contacts_collabs.empty?
 
           render json: AccountContactCollaboratorPresenter.json_collection(contacts_collabs), status: :ok
         end
 
         private
-          def retrieve_contacts
-            AccountContactCollaboratorRepository.contacts_by_account(params[:account_id])
+          def contacts_collabs
+            @contacts_collabs ||= AccountContactCollaboratorRepository.contacts_by_account(params[:account_id])
           end
       end
     end
