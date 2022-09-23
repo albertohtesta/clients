51929fbe9f99024cd78e810da04fb4380c590b06
Bryan <bryan.guerrero@michelada.io>
2022-09-23 12:24:28 -0500

    fix: minitest error with auth requirements

--- test/controllers/api/v1/collaborators_controller_test.rb
+++ test/controllers/api/v1/collaborators_controller_test.rb
@@ -4,7 +4,9 @@ require "test_helper"
 
 class Api::V1::Teams::CollaboratorsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    user = create(:contact, :user)
     stub_cognito_uri
+    login_as(user)
   end
 
   test "should get index" do
