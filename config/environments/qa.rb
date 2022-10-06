0986eb9b29cf34661ad08c30f633cbbd9db69ca1
Aldo Enriquez <aldo.enriquez@michelada.io>
2022-10-06 14:34:58 -0500

    SMTP port config for QA

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
 
   config.mail_from = %(NORDHEN <Info@nordhen.com>)
   config.action_mailer.default_url_options = { host: "nordhen.com" }
-  config.action_mailer.smtp_settings = { address: ENV["SMTP_ADDRESS"], user_name: ENV["SMTP_USER"], password: ENV["SMTP_PASSWORD"] }
+  config.action_mailer.smtp_settings = { port: 587, address: ENV["SMTP_ADDRESS"], user_name: ENV["SMTP_USER"], password: ENV["SMTP_PASSWORD"] }
   config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
