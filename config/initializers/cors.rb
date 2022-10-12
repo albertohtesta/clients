5a80d4b4452b24e98466588f3441a013102905bb
Ivan Osorio De Anda <iosorio@arkusnexus.com>
2022-10-12 04:48:20 +0000

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,17 +2,21 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "qa-clients.nordhen.com",
-            "localhost:4001",
+    origins "localhost:4001",
             "arkusimpel.my.salesforce.com",
             "arkusimpel--restapidev.sandbox.lightning.force.com",
+            "https://qa-clients.nordhen.com",
+            "https://qa-clients-api.nordhen.com",
+            "https://staging-clients.nordhen.com",
+            "https://staging-clients-api.nordhen.com",
+            "https://clients-api.nordhen.com",
+            "https://clients.nordhen.com",
             "https://qa-core.nordhen.com",
             "https://staging-core.nordhen.com",
             "https://core.nordhen.com",
-            "https://qa-core-api.nordhen.com",
-            "https://qa-clients-api.nordhen.com",
-            "https://staging-clients.nordhen.com",
-            "https://clients.nordhen.com"
+            "https://nordhen.com",
+            "https://api.nordhen.com",
+            "https://qa-core-api.nordhen.com"
     resource "*",
       headers: :any,
       methods: %i[get post put patch delete options head]
